d953ca958eeb9bd2703f86c23ecd05216691f5f6
Vasyl Germanskij <rmacciawelli@mail.ru>
2014-04-11 17:30:03 +0300

    pages controller index about info contact download

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def info
+  end
+
+  def download
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Heroku192::Application.routes.draw do
+  get "pages/index"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  get "pages/info"
+
+  get "pages/download"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +58,7 @@ Heroku192::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'pages#index'
 
   # See how all your routes lay out with "rake routes"
 
